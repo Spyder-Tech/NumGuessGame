1336900cd5f3743f822633d9dd3230e35f606a96
Isaiah F. <ikfolkes@aol.com>
2025-04-09 16:15:19 -0400

    Deleted unused imports

--- NumGuessGame/src/main/java/NGG/com/example/NumGuessGame/NumberGuessingGameApplication.java
+++ NumGuessGame/src/main/java/NGG/com/example/NumGuessGame/NumberGuessingGameApplication.java
@@ -1,23 +1,5 @@
 package NGG.com.example.NumGuessGame;
 
-/*
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@SpringBootApplication
-public class NumberGuessingGameApplication implements CommandLineRunner {
-
-	public static void main(String[] args)
-	{
-		SpringApplication.run(NumberGuessingGameApplication.class, args);
-	}
-
-}
-*/
-
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
